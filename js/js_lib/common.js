5bace0b5a44408e8ebc6fed42e9e77396c6d3fb4
wvictor <wvictor@hk1.ibm.com>
2018-01-04 12:48:14 +0800

    Include complete js file

---
+++ js/js_lib/common.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    document.addEventListener("deviceready", onDeviceReadyTouch, false);
+});
+
+function onDeviceReadyTouch() {
+    init3d();
+    startPdfHandler();
+}
+
+function init3d() {
+    ThreeDeeTouch.onHomeIconPressed = function (payload) {
+        if ( (payload.type == 'app') && (payload.title == 'CITICtoken') ) {
+            var url_scheme = 'iDGateCITIC://';
+            // var url_scheme = 'calshow://';
+
+            appAvailability.check(
+                url_scheme,
+                function() {
+                    window.open(url_scheme, '_system');
+                },
+                function() {
+                    window.open("https://itunes.apple.com/app/id1219197546");
+                }
+            );
+        } else if ( (payload.type == 'browser') && (payload.title == 'Corporate Website') ) {
+            window.open('https://www.cncbinternational.com');
+        }
+    }
+}
+
+function startPdfHandler() {
+    window.onclick = function(event) { 
+        if (event.toElement.pathname) {
+            if (event.toElement.pathname.match("pdf$")) {
+                var url = event.toElement.protocol + "\/\/" + event.toElement.hostname + event.toElement.pathname;
+                if (device.platform == 'Android') {
+                    event.preventDefault(); 
+                    navigator.app.loadUrl(url, { openExternal:true });
+                } else if (device.platform == 'iOS') {
+                    event.preventDefault(); 
+                    var ref = cordova.InAppBrowser.open(url, '_blank', 'location=no');                    
+                }
+            }
+        }
+    };
+}
+
+
